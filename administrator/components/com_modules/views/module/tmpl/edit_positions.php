d03b56fbbd989ff4f5c1671bca6a02abbbdcb699
Anderson Grüdtner Martins <andergmartins@gmail.com>
2012-08-02 19:46:03 -0300

    Add translation for module positions select field

--- administrator/components/com_modules/views/module/tmpl/edit_positions.php
+++ administrator/components/com_modules/views/module/tmpl/edit_positions.php
@@ -30,7 +30,8 @@ foreach ($templates as $template)
 	$positions = TemplatesHelper::getPositions($clientId, $template);
 	foreach ($positions as $position)
 	{
-		$options[] = createOption($position);
+		$text = getTranslatedModulePosition($template, $position) . ' [' . $position . ']';
+		$options[] = createOption($position, $text);
 
 		if (!$isTemplatePosition && $this->item->position === $position)
 		{
@@ -38,7 +39,7 @@ foreach ($templates as $template)
 		}
 	}
 
-	$templateGroups[$template] = createOptionGroup($template, $options);
+	$templateGroups[$template] = createOptionGroup(ucfirst($template), $options);
 }
 
 // Add custom position to options
@@ -53,7 +54,7 @@ if (!empty($this->item->position) && !$isTemplatePosition)
 $attr = array(
 	'id'          => 'jform_position',
 	'list.select' => $this->item->position,
-	'list.attr'   => 'class="chzn-custom-value" '
+	'list.attr'   => 'class="chzn-custom-value input-xlarge" '
 		. 'data-custom_group_text="' . $customGroupText . '" '
 		. 'data-no_results_text="' . JText::_('COM_MODULES_ADD_CUSTOM_POSITION') . '" '
 		. 'data-placeholder="' . JText::_('COM_MODULES_TYPE_OR_SELECT_POSITION') . '" '
@@ -61,25 +62,73 @@ $attr = array(
 
 echo JHtml::_('select.groupedlist', $templateGroups, 'jform[position]', $attr);
 
+/**
+ * Return a translated module position name
+ *
+ * @param   string  $template  Template name
+ * @param   string  $position  Position name
+ *
+ * @return  string             Return a translated position name
+ */
+function getTranslatedModulePosition($template, $position)
+{
+	// Template translation
+	$lang = JFactory::getLanguage();
+	$lang->load('tpl_' . $template . '.sys', JPATH_SITE . '/templates/' . $template, $lang->getDefault(), false, false);
+
+	$langKey = strtoupper('TPL_' . $template . '_POSITION_' . $position);
+	$text = JText::_($langKey);
+
+	// Avoid untranslated strings
+	if (!isTranslatedText($langKey, $text))
+	{
+		// Modules component translation
+		$langKey = strtoupper('COM_MODULES_POSITION_' . $position);
+		$text = JText::_($langKey);
+
+		// Avoid untranslated strings
+		if (!isTranslatedText($langKey, $text))
+		{
+			// Try to humanize the position name
+			$text = ucfirst(preg_replace('/^' . $template . '\-/', '', $position));
+			$text = ucwords(str_replace(array('-', '_'), ' ', $text));
+		}
+	}
+
+	return $text;
+}
+
+/**
+ * Check if the string was translated
+ *
+ * @param   string  $langKey  Language file text key
+ * @param   string  $text     The "translated" text to be checked
+ *
+ * @return  boolean           Return true for translated text
+ */
+function isTranslatedText($langKey, $text)
+{
+	return $text !== $langKey;
+}
 
 /**
  * Create and return a new Option
  *
  * @param   string  $value  The option value [optional]
- * @param   string  $label  The option label [optional]
+ * @param   string  $text   The option text [optional]
  *
  * @return  object          The option as an object (stdClass instance)
  */
-function createOption($value = '', $label = '')
+function createOption($value = '', $text = '')
 {
-	if (empty($label))
+	if (empty($text))
 	{
-		$label = $value;
+		$text = $value;
 	}
 
 	$option = new stdClass;
 	$option->value = $value;
-	$option->text  = $label;
+	$option->text  = $text;
 
 	return $option;
 }
