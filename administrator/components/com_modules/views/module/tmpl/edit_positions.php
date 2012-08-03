73185c27548004b595b665ac6069141ace809d74
Anderson Grüdtner Martins <andergmartins@gmail.com>
2012-08-03 00:11:15 -0300

    Move functions from tmpl to modules helper
    - Move functions from tmpl edit_positions for modules to modules helper
    
    - Fix code style for the helper "modules"

--- administrator/components/com_modules/views/module/tmpl/edit_positions.php
+++ administrator/components/com_modules/views/module/tmpl/edit_positions.php
@@ -18,8 +18,8 @@ $templates      = array_keys(ModulesHelper::getTemplates($clientId, $state));
 $templateGroups = array();
 
 // Add an empty value to be able to deselect a module position
-$option = createOption();
-$templateGroups[''] = createOptionGroup('', array($option));
+$option = ModulesHelper::createOption();
+$templateGroups[''] = ModulesHelper::createOptionGroup('', array($option));
 
 // Add positions from templates
 $isTemplatePosition = false;
@@ -30,8 +30,8 @@ foreach ($templates as $template)
 	$positions = TemplatesHelper::getPositions($clientId, $template);
 	foreach ($positions as $position)
 	{
-		$text = getTranslatedModulePosition($template, $position) . ' [' . $position . ']';
-		$options[] = createOption($position, $text);
+		$text = ModulesHelper::getTranslatedModulePosition($template, $position) . ' [' . $position . ']';
+		$options[] = ModulesHelper::createOption($position, $text);
 
 		if (!$isTemplatePosition && $this->item->position === $position)
 		{
@@ -39,7 +39,7 @@ foreach ($templates as $template)
 		}
 	}
 
-	$templateGroups[$template] = createOptionGroup(ucfirst($template), $options);
+	$templateGroups[$template] = ModulesHelper::createOptionGroup(ucfirst($template), $options);
 }
 
 // Add custom position to options
@@ -47,7 +47,7 @@ $customGroupText = JText::_('COM_MODULES_CUSTOM_POSITION');
 if (!empty($this->item->position) && !$isTemplatePosition)
 {
 	$option = createOption($this->item->position);
-	$templateGroups[$customGroupText] = createOptionGroup($customGroupText, array($option));
+	$templateGroups[$customGroupText] = ModulesHelper::createOptionGroup($customGroupText, array($option));
 }
 
 // Build field
@@ -61,92 +61,3 @@ $attr = array(
 );
 
 echo JHtml::_('select.groupedlist', $templateGroups, 'jform[position]', $attr);
-
-/**
- * Return a translated module position name
- *
- * @param   string  $template  Template name
- * @param   string  $position  Position name
- *
- * @return  string             Return a translated position name
- */
-function getTranslatedModulePosition($template, $position)
-{
-	// Template translation
-	$lang = JFactory::getLanguage();
-	$lang->load('tpl_' . $template . '.sys', JPATH_SITE . '/templates/' . $template, $lang->getDefault(), false, false);
-
-	$langKey = strtoupper('TPL_' . $template . '_POSITION_' . $position);
-	$text = JText::_($langKey);
-
-	// Avoid untranslated strings
-	if (!isTranslatedText($langKey, $text))
-	{
-		// Modules component translation
-		$langKey = strtoupper('COM_MODULES_POSITION_' . $position);
-		$text = JText::_($langKey);
-
-		// Avoid untranslated strings
-		if (!isTranslatedText($langKey, $text))
-		{
-			// Try to humanize the position name
-			$text = ucfirst(preg_replace('/^' . $template . '\-/', '', $position));
-			$text = ucwords(str_replace(array('-', '_'), ' ', $text));
-		}
-	}
-
-	return $text;
-}
-
-/**
- * Check if the string was translated
- *
- * @param   string  $langKey  Language file text key
- * @param   string  $text     The "translated" text to be checked
- *
- * @return  boolean           Return true for translated text
- */
-function isTranslatedText($langKey, $text)
-{
-	return $text !== $langKey;
-}
-
-/**
- * Create and return a new Option
- *
- * @param   string  $value  The option value [optional]
- * @param   string  $text   The option text [optional]
- *
- * @return  object          The option as an object (stdClass instance)
- */
-function createOption($value = '', $text = '')
-{
-	if (empty($text))
-	{
-		$text = $value;
-	}
-
-	$option = new stdClass;
-	$option->value = $value;
-	$option->text  = $text;
-
-	return $option;
-}
-
-/**
- * Create and return a new Option Group
- *
- * @param   string  $label    Value and label for group [optional]
- * @param   array   $options  Array of options to insert into group [optional]
- *
- * @return  array             Return the new group as an array
- */
-function createOptionGroup($label = '', $options = array())
-{
-	$group = array();
-	$group['value'] = $label;
-	$group['text']  = $label;
-	$group['items'] = $options;
-
-	return $group;
-}
