b0ce4ed5d48eb2c0bf528d29c81dd760128a58e9
kyleledbetter <kyle@kyleledbetter.com>
2012-08-15 11:02:07 -0500

    - updating html xml and lang variables and making sure html5 is used on component.php's

--- administrator/templates/isis/component.php
+++ administrator/templates/isis/component.php
@@ -30,7 +30,7 @@ if (is_file($file)) :
 endif;
 
 ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
 	<script src="../media/jui/js/jquery.js"></script>
--- templates/protostar/index.php
+++ templates/protostar/index.php
@@ -70,7 +70,7 @@ else
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php echo $this->language; ?>" lang="<?php echo $this->language; ?>" >
 <head>
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<?php
